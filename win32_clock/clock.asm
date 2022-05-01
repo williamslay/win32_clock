0da450b8993ac0de5a52454181dafcd3298934b3
WilliamsLay <WilliamsLay.lsy@outlook.com>
2022-05-01 21:07:56 +0800

    0.1.5 Menu and Acclerator

--- win32_clock/clock.asm
+++ win32_clock/clock.asm
@@ -12,15 +12,16 @@ include kernel32.inc
 includelib kernel32.lib
 
 ; Equ 等值定义
-IDR_MENU1 equ 2000h;菜单
-IDI_ICON1 equ 1000h;图标
+IDI_ICON equ 1000h;图标
+IDR_MENU equ 2000h;菜单
+IDR_ACCELERATOR equ 2000h;加速键
+
 
 ; 数据段
 .data?
 hInstance dd ?
 hWinMain dd ?
 hMenu dd ?
-hIcon dd ?
 
 ; 常量
 .const
@@ -51,14 +52,17 @@ _ProcWinMain endp
 _WinMain proc
     local @stWndClass:WNDCLASSEX
     local @stMsg:MSG
+    local @hAccelerator
 
     invoke GetModuleHandle,NULL
     mov hInstance,eax
 
     ;载入菜单
-    ;invoke	LoadMenu,hInstance,IDR_MENU1
-	;mov	hMenu,eax
-    
+    invoke	LoadMenu,hInstance,IDR_MENU
+	mov	hMenu,eax
+    ;载入加速键
+    invoke	LoadAccelerators,hInstance,IDR_ACCELERATOR
+	mov	@hAccelerator,eax
 
     ;-------注册窗口类---------
     ;局部变量全0
@@ -68,7 +72,7 @@ _WinMain proc
     invoke LoadCursor,0,IDC_ARROW
     mov @stWndClass.hCursor,eax
     ;加载图标
-    invoke	LoadIcon,hInstance,IDI_ICON1
+    invoke	LoadIcon,hInstance,IDI_ICON
     mov	@stWndClass.hIcon,eax
     push hInstance
     pop @stWndClass.hInstance
@@ -83,7 +87,7 @@ _WinMain proc
         offset szClassName,offset szCaptionMain, \
         WS_OVERLAPPEDWINDOW, \
         800,500,600,400, \
-        NULL,NULL,hInstance,NULL
+        NULL,hMenu,hInstance,NULL
     mov hWinMain,eax
     invoke ShowWindow,hWinMain,SW_SHOWNORMAL
     invoke UpdateWindow,hWinMain
@@ -91,8 +95,11 @@ _WinMain proc
     .while TRUE
         invoke GetMessage,addr @stMsg,NULL,0,0
         .break .if eax == 0
+        invoke	TranslateAccelerator,hWinMain,@hAccelerator,addr @stMsg
+        .if eax == 0
         invoke TranslateMessage,addr @stMsg
         invoke DispatchMessage,addr @stMsg
+        .endif
     .endw
     ret
 _WinMain endp
--- win32_clock/resource.h
+++ win32_clock/resource.h
@@ -2,21 +2,30 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 menu.rc 使用
 //
-#define IDI_ICON1                       0x1000
-#define IDR_MENU1                       0x2000
+#define IDI_ICON                        0x1000
+#define IDR_MENU                        0x2000
+#define IDR_ACCELERATOR                 0x2000
+#define New_Clock                       0x4001
+#define Exsit_Clock                     0x4002
+#define Exit                            0x4003
 #define ID_40001                        40001
 #define ID_40002                        40002
 #define ID_40003                        40003
 #define ID_40004                        40004
 #define ID_40005                        40005
 #define ID_40006                        40006
+#define ID_40007                        40007
+#define ID_40008                        40008
+#define ID_40009                        40009
+#define ID_40010                        40010
+
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40021
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
